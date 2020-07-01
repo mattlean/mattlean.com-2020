16aa0e9bbc6648371698570d74e9fe7612095711
Matthew Lean <matt@mattlean.com>
2020-07-01 09:05:35 -0700

    Use common data for Landing page content

--- src/common/postData.js
+++ src/common/postData.js
@@ -1,9 +1,16 @@
+import FeaturedCSLeanSpace from '../front/assets/lean-space-featured-project.jpg'
+import FeaturedCSSOT from '../front/assets/spectral-overlay-tool-featured-project.jpg'
+
 const ORDER = ['cs-ml2020', 'cs-sot', 'cs-lean-space', 'hello-world']
 const POSTS = {
   'cs-lean-space': {
     title: 'Case Study: Lean Space',
     subtitle: 'Learnings from building my personal website in 2015.',
     published: 'June 30, 2020',
+    featured: {
+      img: FeaturedCSLeanSpace,
+      alt: "Featured image for Lean Space case study: the project's logo",
+    },
     tags: ['case-study', 'design', 'frontend'],
   },
   'cs-ml2020': {
@@ -16,6 +23,11 @@ const POSTS = {
     title: 'Case Study: Spectral Overlay Tool',
     subtitle: 'Building a biotech data visualization tool.',
     published: 'June 30, 2020',
+    featured: {
+      img: FeaturedCSSOT,
+      alt:
+        "Featured image for Spectral Overlay Tool case study: a screenshot of one of the tool's charts",
+    },
     tags: ['case-study', 'design', 'frontend', 'tooling'],
   },
   'hello-world': {
