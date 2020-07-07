87ceade03f7936d755ba00ab44586be2e9aa39f5
Matthew Lean <matt@mattlean.com>
2020-07-06 23:10:51 -0700

    Progress on responsive design

--- src/front/components/MainFooter/index.jsx
+++ src/front/components/MainFooter/index.jsx
@@ -127,7 +127,7 @@ const MainFooter = () => {
         variants={variants}
         className="txt-xs txt-grey-c"
       >
-        Made with too much boba tea in SF Bay Area.{' '}
+        Made with too much boba tea in SF Bay Area. <br className="d-sm" />
         <a href="#" className="link-grey-c">
           Learn more about this site.
         </a>
