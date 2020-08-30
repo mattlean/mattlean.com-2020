b8c2302708e5c1ad3203a9c71ad216d8125103dd
Matthew Lean <matt@mattlean.com>
2020-08-30 00:31:07 -0700

    Update About & Project pages - Adjust bounding box for About & Project overview pages - Update ALWP to use NewBlind & focus on route change

--- src/front/pages/Projects/pages/ALWP.jsx
+++ src/front/pages/Projects/pages/ALWP.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Badge, Btn } from 'eswiss'
 import BlindFrame from '../../../components/Blind/BlindFrame'
+import NewBlindFrame from '../../../components/Blind/NewBlindFrame'
 import { getProjectData } from '../../../../common/data/project'
+import {
+  setupBlindObservers,
+  useInitAnim,
+} from '../../../components/Blind/initAnimUtil'
 import { useHeadDataEffect } from '../../../util'
 import NPMIcon from '../../../assets/icons/npm.svg'
 import { MarkGithubIcon } from '@primer/octicons-react'
@@ -13,9 +18,45 @@ const ALWP = () => {
   useHeadDataEffect()
   const { name, tags } = getProjectData('alwp')
 
+  const {
+    blindVisibleStates,
+    blindStates,
+    initAnimComplete,
+    observerData,
+    runInitAnim,
+  } = useInitAnim(5)
+
+  // Setup effect which is only run once
+  useEffect(() => {
+    // Focus starting element on page load
+    if (observerData[0].ref.current) observerData[0].ref.current.focus()
+
+    const observers = setupBlindObservers(
+      [0.5, 0.1, 0.1, 0.1, 0.1],
+      observerData,
+      blindVisibleStates
+    )
+
+    window.setTimeout(runInitAnim, 100)
+
+    // Disconnect all observers on unmount
+    return () => observers.forEach((observer) => observer.disconnect())
+  }, [])
+
   return (
     <>
-      <BlindFrame nodeType="header" className="cover">
+      <NewBlindFrame
+        ref={observerData[0].ref}
+        nodeType="header"
+        delay={blindStates[0].delay}
+        observer={observerData[0].observer}
+        play={
+          initAnimComplete
+            ? blindVisibleStates[0].isVisible
+            : blindStates[0].play
+        }
+        className="cover"
+      >
         <h1 className="h-1 md:h-2 sm:h-3">{name}</h1>
         <ul className="badge-list">
           {tags.map((t) => (
@@ -24,11 +65,33 @@ const ALWP = () => {
             </Badge>
           ))}
         </ul>
-      </BlindFrame>
-      <BlindFrame nodeType="h2" className="project-overview h-2 md:h-4">
+      </NewBlindFrame>
+      <NewBlindFrame
+        ref={observerData[1].ref}
+        nodeType="h2"
+        delay={blindStates[1].delay}
+        observer={observerData[1].observer}
+        play={
+          initAnimComplete
+            ? blindVisibleStates[1].isVisible
+            : blindStates[1].play
+        }
+        className="project-overview h-2 md:h-4"
+      >
         Project Overview
-      </BlindFrame>
-      <BlindFrame nodeType="section" className="subgrid-project-desc grid">
+      </NewBlindFrame>
+      <NewBlindFrame
+        ref={observerData[2].ref}
+        nodeType="section"
+        delay={blindStates[2].delay}
+        observer={observerData[2].observer}
+        play={
+          initAnimComplete
+            ? blindVisibleStates[2].isVisible
+            : blindStates[2].play
+        }
+        className="subgrid-project-desc grid"
+      >
         <section className="c-grey-1">
           <p>
             <strong>Asset List Webpack Plugin</strong> is an open source{' '}
@@ -68,13 +131,32 @@ const ALWP = () => {
             <NPMIcon className="btn-icon" /> npm
           </Btn>
         </a>
-      </BlindFrame>
-      <BlindFrame nodeType="h3" className="project-details-header">
+      </NewBlindFrame>
+      <NewBlindFrame
+        ref={observerData[3].ref}
+        nodeType="h3"
+        delay={blindStates[3].delay}
+        observer={observerData[3].observer}
+        play={
+          initAnimComplete
+            ? blindVisibleStates[3].isVisible
+            : blindStates[3].play
+        }
+        className="project-details-header"
+      >
         Project Details
-      </BlindFrame>
-      <BlindFrame
+      </NewBlindFrame>
+      <NewBlindFrame
+        ref={observerData[4].ref}
         nodeType="section"
         className="subgrid-project-details grid mb-0 c-grey-1"
+        delay={blindStates[4].delay}
+        observer={observerData[4].observer}
+        play={
+          initAnimComplete
+            ? blindVisibleStates[4].isVisible
+            : blindStates[4].play
+        }
       >
         <section>
           <section>
@@ -128,7 +210,7 @@ const ALWP = () => {
             </ul>
           </section>
         </section>
-      </BlindFrame>
+      </NewBlindFrame>
     </>
   )
 }
