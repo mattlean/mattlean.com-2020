38de400172f10fcf4aeb0f6e7c0d24c8bfd2ec29
Matthew Lean <matt@mattlean.com>
2020-08-29 19:45:28 -0700

    Update general styles, update Contact page - Update spacing for dd eles - Refactor Contact page to use NewBlindFrame

--- src/front/pages/Contact/index.jsx
+++ src/front/pages/Contact/index.jsx
@@ -1,8 +1,13 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { motion } from 'framer-motion'
 import BlindFrame from '../../components/Blind/BlindFrame'
+import NewBlindFrame from '../../components/Blind/NewBlindFrame'
 import MainFooter from '../../components/MainFooter'
 import VARIANTS from '../page_variants'
+import {
+  setupBlindObservers,
+  useInitAnim,
+} from '../../components/Blind/initAnimUtil'
 import { useHeadDataEffect } from '../../util'
 import { MailIcon } from '@primer/octicons-react'
 import LogoLinkedIn from '../../assets/icons/linkedin.png'
@@ -13,10 +18,29 @@ import LogoLinkedIn from '../../assets/icons/linkedin.png'
 const Contact = () => {
   useHeadDataEffect()
 
-  // Focus starting element on page load
-  const srStart = useRef(null)
+  const {
+    blindVisibleStates,
+    blindStates,
+    initAnimComplete,
+    observerData,
+    runInitAnim,
+  } = useInitAnim(4)
+
+  // Setup effect which is only run once
   useEffect(() => {
-    if (srStart.current) srStart.current.focus()
+    // Focus starting element on page load
+    if (observerData[0].ref.current) observerData[0].ref.current.focus()
+
+    const observers = setupBlindObservers(
+      [0.5, 0.1, 0.1, 0.1],
+      observerData,
+      blindVisibleStates
+    )
+
+    window.setTimeout(runInitAnim, 100)
+
+    // Disconnect all observers on unmount
+    return () => observers.forEach((observer) => observer.disconnect())
   }, [])
 
   return (
@@ -30,32 +54,66 @@ const Contact = () => {
     >
       <main aria-label="Content" className="grid-contact grid">
         <section className="cover">
-          <BlindFrame
-            ref={srStart}
+          <NewBlindFrame
+            ref={observerData[0].ref}
             nodeType="h1"
+            delay={blindStates[0].delay}
+            observer={observerData[0].observer}
+            play={
+              initAnimComplete
+                ? blindVisibleStates[0].isVisible
+                : blindStates[0].play
+            }
             tabIndex="-1"
             className="h-2 sm:h-3 lh-1"
           >
             Let’s get in&nbsp;touch.
-          </BlindFrame>
-          <BlindFrame
+          </NewBlindFrame>
+          <NewBlindFrame
+            ref={observerData[1].ref}
             nodeType="p"
-            delay={0.5}
+            delay={blindStates[1].delay}
+            observer={observerData[1].observer}
+            play={
+              initAnimComplete
+                ? blindVisibleStates[1].isVisible
+                : blindStates[1].play
+            }
             className="txt-8 sm:txt-6 c-grey-1"
           >
             Have any questions? Got a project in mind for&nbsp;me?
             <br />
             Let’s connect and start&nbsp;talking.
-          </BlindFrame>
+          </NewBlindFrame>
         </section>
         <ul>
-          <BlindFrame nodeType="li" delay={0.5}>
+          <NewBlindFrame
+            ref={observerData[2].ref}
+            nodeType="li"
+            delay={blindStates[2].delay}
+            observer={observerData[2].observer}
+            play={
+              initAnimComplete
+                ? blindVisibleStates[2].isVisible
+                : blindStates[2].play
+            }
+          >
             <a href="mailto:matt@mattlean.com" className="a-primary">
               <MailIcon size={24} />
               matt@mattlean.com
             </a>
-          </BlindFrame>
-          <BlindFrame nodeType="li" delay={0.5}>
+          </NewBlindFrame>
+          <NewBlindFrame
+            ref={observerData[3].ref}
+            nodeType="li"
+            delay={blindStates[3].delay}
+            observer={observerData[3].observer}
+            play={
+              initAnimComplete
+                ? blindVisibleStates[3].isVisible
+                : blindStates[3].play
+            }
+          >
             <a
               href="https://linkedin.com/in/mattlean"
               rel="noreferrer"
@@ -65,7 +123,7 @@ const Contact = () => {
               <img src={LogoLinkedIn} alt="LinkedIn Icon" />
               linkedin.com/in/mattlean
             </a>
-          </BlindFrame>
+          </NewBlindFrame>
         </ul>
       </main>
       <MainFooter />
